7983a4de930d49a9ba060f48f5f60a2680cb0aa7
George Lesica <george.lesica@workiva.com>
2017-01-10 13:19:51 -0700

    Fix the number of companies and add a test.

--- lib/src/companies.dart
+++ lib/src/companies.dart
@@ -2,11 +2,13 @@ import 'dart:math';
 
 import 'package:fortune_1000_companies/src/companies_data.dart';
 
-/// List of all 1000 companies.
-final List<String> companies = companiesString.split(',');
+final Random _random = new Random();
+
+/// Immutable list of all 1000 companies.
+final Iterable<String> companies = companiesString.split('|');
 
 /// Pulls a random company from the list of 1000.
 String getRandomCompanyName() {
-  int _randomCompanyIndex = new Random().nextInt(companies.length - 1);
-  return (companies.toList())[_randomCompanyIndex];
+  int index = _random.nextInt(companies.length - 1);
+  return companies.elementAt(index);
 }
